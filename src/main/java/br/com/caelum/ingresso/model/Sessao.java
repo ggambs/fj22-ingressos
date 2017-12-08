31af38af5840d09c85d6328987cd2d12c5eff03f
GabrielGambarini <ggambarini22@hotmail.com>
2017-12-08 19:48:15 -0200

    Adicionando tela de lugares, exercicio 5.2

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,6 +2,8 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -46,6 +48,11 @@ public class Sessao {
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
 	}
+	
+	public Map<String, List<Lugar>> getMapaDeLugares(){
+		return sala.getMapaDeLugares();
+	}
+	
 
 	public Integer getId() {
 		return id;
